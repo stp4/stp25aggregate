e6a275219348b3276b529af24c4a0387393c758d
stp4 <w.peter@statistik-peter.at>
2019-05-21 06:20:59 +0200

    Neue Funktion Dapply

--- R/dapply2.R
+++ R/dapply2.R
@@ -1,3 +1,8 @@
+# Ueberarbeitet am  "Sun Apr 14 10:39:13 2019"
+
+
+
+
 #' Apply mit Hmisc labels
 #'
 #' @description  Mischung usd der \code{llply} und \code{data.frame} -Funktion.
@@ -26,3 +31,56 @@ dapply2 <- function (.data,
                                 stringsAsFactors=stringsAsFactors),
                      GetLabelOrName(.data))
 }
+
+
+#' Make New Variables
+#' 
+#' Apply mit formula
+#'
+#' @param x Objekt
+#' @param ... an dapply2
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' library(stp25aggregate)
+#' library(stp25data)
+#' res <- Dapply(~ a  +  gr  + gew+chol0+chol1+chol6, hyper, fun=function(x) cut(x,3))
+#' head(res)
+#'
+#'
+Dapply <- function(x, ...) {
+  UseMethod("Dapply")
+}
+
+
+#' @rdname dapply2
+#' @export
+Dapply.formula <- function(x,
+                          .data,
+                          fun = function(x)
+                            as.numeric(x),
+                          stringsAsFactors = default.stringsAsFactors()) {
+  X <- stp25formula::prepare_data2(x, .data)
+  apply_data <- dapply2(X$data[X$measure.vars], fun)
+  
+  .data[, X$measure.vars] <- apply_data
+  .data
+}
+
+#' @rdname dapply2
+#' @export
+Dapply.data.frame <- function(.data,
+                             ...,
+                             fun = function(x)
+                               as.numeric(x),
+                             stringsAsFactors = default.stringsAsFactors()) {
+  X <- stp25formula::prepare_data2(.data, ...)
+  apply_data <- dapply2(X$data[X$measure.vars], fun)
+  
+  .data[, X$measure.vars] <- apply_data
+  .data
+}
+
