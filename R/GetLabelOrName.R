3e7685b600c0c59176ec2bee4b637c00c316de53
stp4 <w.peter@statistik-peter.at>
2019-06-16 20:55:21 +0200

    vereinfacht

--- R/GetLabelOrName.R
+++ R/GetLabelOrName.R
@@ -14,36 +14,36 @@ GetLabelOrName <- function(x,
                            replacement = " ") {
   if (length(x) < 1) {
     return(NULL)
-  }else {
+  } else {
     xnames  <- names(x)
     # xlabel <- if( Hmisc_label) Hmisc::label(x) else xnames
-    xlabel <- Hmisc::label(x)
-    df_names <-  if (is.null(pattern))
-      xnames
-    else
-      gsub(" $","",
-           gsub(pattern, replacement, xnames), perl = TRUE)
+    xlabel <-  get_label(x) # Hmisc::label(x)
+    df_names <- if (is.null(pattern)) xnames
+                else gsub(" $", "", gsub(pattern, replacement, xnames), 
+                          perl = TRUE)
     #   gsub(pattern, replacement, names(x), perl=T )
-    xlabel <-
-      ifelse(xlabel == "", df_names, xlabel)
-
+   # xlabel <- ifelse(xlabel == "", df_names, xlabel)
     
     is_units <- sapply(x, function(z) inherits(z, "units"))
     if (any(is_units)) {
       my_units <-
         sapply(x, function(z)
           if (inherits(z, "units"))
-            paste0(" [", as.character(attr(z, "units")), "]"  )
-          else "")
-      
-      xlabel <-  paste0(xlabel, my_units )
-      
+            paste0(" [", as.character(attr(z, "units")), "]")
+          else
+            "")
+      xlabel <-  paste0(xlabel, my_units)
+      names(xlabel) <- xnames
     }
-    
-    names(xlabel) <- xnames
-    return(xlabel)
   }
+  xlabel
 }
 
 
-GetLabels <- GetLabelOrName
+get_label <- function(x) {
+  lbl <- lapply(x, attr, "label")
+  unlabl <- which(sapply(lbl, is.null))
+  lbl[unlabl] <- names(lbl[unlabl])
+  unlist(lbl)
+  
+}
