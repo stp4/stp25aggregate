c075198857bd4b5593d99dd4e66b9707ca3410d6
stp4 <w.peter@statistik-peter.at>
2020-02-18 06:47:14 +0100

    Levels und Labels anzeigen.

--- R/Codebook.R
+++ R/Codebook.R
@@ -1,24 +1,64 @@
+
+
+#' Codebook
+#' 
 #' Extrahier die Daten-Struktur
-
-
-#' @rdname upData2
-#' @description Die Funltion \code{Codebook()} Extrahier die Daten-Struktur
+#' @param data data.frame
+#'
+#' @param abbreviate Abkuerzen del Levels
+#'
 #' @export
-Codebook<- function(data){
-  lbs <- get_label(data)
-  lvl<- sapply(data,
-               function(x) {
-                 if(is.factor(x)){
-                   lvl <- levels(x)
-                   if(length(grep(";" , lvl)) != 0 ) "Warning: ; in levels gefunden"
-                   else
-                     paste0(lvl, collapse=";")
-                 }
-                 else  class(x)[1]
-                 
-               })
-  cbind(names=names(lbs),
-        labels=lbs  ,
-        levels=as.vector(lvl) )
-  
-}
+Codebook <-
+  function(data, abbreviate = TRUE) {
+    n <- nrow(data)
+    lbs <- get_label(data)
+    lvl <- sapply(data,
+                  function(x) {
+                    if (is.factor(x)) {
+                      lvl <- levels(x)
+                      if (abbreviate)
+                        lvl <- abbreviate(lvl, 
+                                          use.classes = FALSE)
+                      if (length(grep("\\|" , lvl)) != 0)
+                        "Warning: ; in levels gefunden"
+                      else
+                        paste0(nlevels(x), " levels ", 
+                               paste0(' "', 
+                                      lvl, '"', 
+                                      collapse = ','))
+                    }
+                    else
+                      class(x)[1]
+                    
+                  })
+    
+    n_data <- sapply(data,
+                     function(x) {
+                       length(na.omit(x))
+                     })
+    
+    stat_data <- sapply(data,
+                        function(x) {
+                          if (is.factor(x))
+                            paste(table(x), 
+                                  collapse = "|")
+                          else if (is.character(x))
+                            "character"
+                          else
+                            paste(
+                              fivenum(
+                                signif(as.numeric(x), 2), 
+                                na.rm = TRUE),
+                              collapse = "; ")
+                        })
+    data.frame(
+      names = names(lbs),
+      labels = lbs,
+      levels = as.vector(lvl),
+      stat = stat_data,
+      missing = (n - n_data),
+      n = n_data
+    )
+  }
+
+ 
